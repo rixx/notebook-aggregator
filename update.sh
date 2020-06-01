6c67ed27174e851278281551d2c76a133665369c
Tobias Kunze <r@rixx.de>
2020-06-01 21:21:57 +0200

    shut it

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
 cd $0:A(:h)
-git pull origin master
+git pull origin master -q
 /home/rixx/.virtualenvs/notebook-aggregator/bin/pip install -Ue .
 /home/rixx/.virtualenvs/notebook-aggregator/bin/notebooks build
