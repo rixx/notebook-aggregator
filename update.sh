0ef619bd9b3138d887dae56a0ea528f8357cb60a
Tobias Kunze <r@rixx.de>
2022-02-19 13:53:34 +0100

    Fix deployment, pt I

--- update.sh
+++ update.sh
@@ -2,5 +2,5 @@
 
 cd $0:A(:h)
 git pull origin master -q
-/home/rixx/.virtualenvs/notebook-aggregator/bin/pip install -Ue .
-/home/rixx/.virtualenvs/notebook-aggregator/bin/notebooks build
+/home/rixx/.local/share/virtualenvs/notebook-aggregator/bin/pip install -Ue .
+/home/rixx/.local/share/virtualenvs/notebook-aggregator/bin/notebooks build
